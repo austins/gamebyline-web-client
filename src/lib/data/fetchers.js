a046927593cece7e9a76b24c31cf7c379d88ee1c
austins <austins@users.noreply.github.com>
2021-06-29 22:48:45 -0700

    Async fetchers and improved error handling

--- src/lib/data/fetchers.js
+++ src/lib/data/fetchers.js
@@ -1,11 +1,31 @@
 import { GraphQLClient, request } from 'graphql-request';
 import isString from 'lodash/isString';
 import set from 'lodash/set';
+import { StatusCodes } from 'http-status-codes';
 
-export const restFetcher = url => fetch(url).then(res => res.json());
+export const restFetcher = async url => {
+    const res = await fetch(url);
 
-export const graphqlFetcher = (query, variables = {}) =>
-    request(process.env.NEXT_PUBLIC_API_GRAPHQL_URL, query, variables);
+    if (!res.ok) {
+        const error = new Error('An error occurred while fetching the REST data.');
+        error.info = await res.json();
+        error.status = res.status;
+        throw error;
+    }
+
+    return res.json();
+};
+
+export const graphqlFetcher = async (query, variables = {}) => {
+    try {
+        return await request(process.env.NEXT_PUBLIC_API_GRAPHQL_URL, query, variables);
+    } catch (errorCaught) {
+        const error = new Error('An error occurred while fetching the GraphQL data.');
+        error.info = errorCaught;
+        error.status = StatusCodes.INTERNAL_SERVER_ERROR;
+        throw error;
+    }
+};
 
 export const getGraphqlClient = (cookieStr = null) => {
     const options = {};
